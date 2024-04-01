0a3175e26ef63e80986687136cb1184f958d9ca7
Angela <avenable11@ivytech.edu>
2024-04-01 14:49:41 -0400

    char and string comparison

--- 04_01_24/main.cpp
+++ 04_01_24/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <cctype>
+#include <algorithm>
 
 int main()
 {
     int magicNum = 79;
     int userNum = 0;
+    char yOrN;
+    std::string food;
 
     std::cout << "Enter a number between 1 and 100: ";
     std::cin >> userNum;
@@ -18,6 +22,24 @@ int main()
     }
 
     std::cout << "Thanks for playing!" << std::endl;
+
+    std::cout << "Did you have fun? ";
+    std::cin >> yOrN;
+    yOrN = tolower(yOrN);
+    if (yOrN == 'y')
+    {
+        std::cout << "Don't forget to like and subscribe." << std::endl;
+    }
+
+    std::cout << "What is your favorite food? ";
+    std::cin >> std::ws;
+    getline(std::cin, food);
+    std::string foodCpy = food;
+    std::transform(food.begin(), food.end(), foodCpy.begin(), ::toupper);
+    if (foodCpy == "PIZZA")
+    {
+        std::cout << "I love pizza too!" << std::endl;
+    }
 
     return 0;
 }
