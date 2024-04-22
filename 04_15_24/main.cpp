440429001d06bffa1f8bc7dd9c94978cc6bce600
Angela <avenable11@ivytech.edu>
2024-04-22 18:22:54 -0400

    rest of function demo

--- 04_15_24/main.cpp
+++ 04_15_24/main.cpp
@@ -2,10 +2,14 @@
 #include <cmath>
 #include <string>
 #include <cctype>
+#include <limits>
 
 int getPositiveInt(std::string prompt);
 char getByteUnit();
 long calculateMemoryCells(int memSize, char unit);
+int secret(int x);
+void resetStream();
+void memInByteUnits(int mar, int &memSize, char &unit);
 
 int main()
 {
@@ -21,9 +25,19 @@ int main()
     std::cout << "The minimum memory address size for " << memCells << " bytes of memory is " << mar << " bits." << std::endl;
 
     mar = getPositiveInt("What is the size of the mar in bits? ");
-
-    memCells = exp2(mar);
-    std::cout << "The maximum memory cells for a " << mar << " bit mar is " << memCells << std::endl;
+    memInByteUnits(mar, memSize, byteUnit);
+    if (byteUnit == 'B')
+    {
+        memCells = exp2(mar);
+        std::cout << "The maximum memory cells for a " << mar << " bit mar is " << memCells << std::endl;
+    }
+    else
+    {
+        std::cout << "The maximum amount of memory for a " << mar << " bit mar is " << memSize << byteUnit << "B" << std::endl;
+    }
+    int x = secret(2);
+    x = secret(-63);
+    x = secret(6);
     return 0;
 }
 
@@ -34,7 +48,17 @@ int getPositiveInt(std::string prompt)
     std::cin >> num;
     std::cout << std::endl;
 
-    // input validation
+    while (!std::cin || num <= 0)
+    {
+        if (!std::cin)
+        {
+            resetStream();
+        }
+        std::cout << "Please enter a number greater than 0." << std::endl;
+        std::cout << prompt;
+        std::cin >> num;
+        std::cout << std::endl;
+    }
 
     return num;
 }
@@ -81,4 +105,47 @@ long calculateMemoryCells(int memSize, char unit)
     long memCells = memSize * pow(2, exponent);
 
     return memCells;
+}
+
+int secret(int x)
+{
+    if (x > 5)
+        return 2 * x;
+    return x;
+}
+
+void resetStream()
+{
+    std::cout << "You did not enter a number. " << std::endl;
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+void memInByteUnits(int mar, int &memSize, char &unit)
+{
+    if (mar >= 40)
+    {
+        mar = mar - 40;
+        unit = 'T';
+    }
+    else if (mar >= 30)
+    {
+        mar = mar - 30;
+        unit = 'G';
+    }
+    else if (mar >= 20)
+    {
+        mar = mar - 20;
+        unit = 'M';
+    }
+    else if (mar >= 10)
+    {
+        mar = mar - 10;
+        unit = 'K';
+    }
+    else
+    {
+        unit = 'B';
+    }
+    memSize = pow(2, mar);
 }
