5bf408a60e53d85e36b3c2f9ddcdc56b188a36c6
Angela Venable <avenable11@ivytech.edu>
2024-04-24 14:06:19 -0400

    binary program before major changes

---
+++ 04_24_24/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <limits>
+#include <cmath>
+
+const int LIST_SIZE = 100;
+void resetStream();
+
+int main()
+{
+    int binaryDigit;
+    int convertedNum = 0;
+    int position = 0;
+    int list[LIST_SIZE];
+
+    while (binaryDigit != -1)
+    {
+        std::cout << "Enter the " << position + 1;
+        if (position == 0)
+        {
+            std::cout << "st";
+        }
+        else if (position == 1)
+        {
+            std::cout << "nd";
+        }
+        else if (position == 2)
+        {
+            std::cout << "rd";
+        }
+        else
+        {
+            std::cout << "th";
+        }
+        std::cout << " binary digit (Enter -1 to complete the conversion): ";
+        std::cin >> binaryDigit;
+        if (binaryDigit == -1)
+        {
+            break;
+        }
+        if (!std::cin)
+        {
+            resetStream();
+            continue;
+        }
+        else if (binaryDigit != 0 && binaryDigit != 1)
+        {
+            std::cout << "A binary digit can only be a 1 or 0." << std::endl;
+            continue;
+        }
+        convertedNum = convertedNum + binaryDigit * pow(2, position);
+        list[position] = binaryDigit;
+        position++;
+    }
+    for (int i = position - 1; i >= 0; i--)
+    {
+        std::cout << list[i];
+    }
+    std::cout << " converted to decimal is " << convertedNum << std::endl;
+
+    // standard array processing for loop
+    /*  for (int i = 0; i < LIST_SIZE; i++)
+     {
+     } */
+
+    return 0;
+}
+
+void resetStream()
+{
+    std::cout << "You entered something that is not a number." << std::endl;
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
