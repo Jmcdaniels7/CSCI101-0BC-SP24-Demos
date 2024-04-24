20a143bdb8429c9b760870f0738c7f6da798c8a8
Angela Venable <avenable11@ivytech.edu>
2024-04-24 15:59:01 -0400

    enum and array

--- 04_24_24/main.cpp
+++ 04_24_24/main.cpp
@@ -1,15 +1,51 @@
 #include <iostream>
 #include <limits>
 #include <cmath>
-
+#include <string>
+enum userTaskType
+{
+    ANDROID_STUDIO,
+    VISUAL_STUDIO_CODE,
+    GAMING,
+    WEB,
+    MULTIPLE_PROGRAM,
+    ZOOM,
+    EMAIL,
+    DOCS,
+    EVERYTHING
+};
 const int LIST_SIZE = 10;
+const int NUM_TASKS = 9;
+const userTaskType tasks[NUM_TASKS] = {ANDROID_STUDIO,
+                                       VISUAL_STUDIO_CODE,
+                                       GAMING,
+                                       WEB,
+                                       MULTIPLE_PROGRAM,
+                                       ZOOM,
+                                       EMAIL,
+                                       DOCS,
+                                       EVERYTHING};
+const std::string taskToStr[NUM_TASKS] = {"Android Studio",
+                                          "Visual Studio Code",
+                                          "Gaming",
+                                          "Web Browsing with Chrome",
+                                          "Multiple Programming Tasks (both Android Studio and Visual Studio Code)",
+                                          "Zoom",
+                                          "Email"
+                                          "Create and edit documents",
+                                          "All Tasks"};
+
 void resetStream();
 void binNumConvert();
 // write a loop using the list in main that finds the sum and average of the numbers
+void sortList(int list[], int listSize);
+void findLargest(int list[], int &largestPos, int listSize);
+void printList(int list[], int listSize);
 
 int main()
 {
     int list[LIST_SIZE];
+    int numElem = 0;
     // standard array processing for loop
     for (int i = 0; i < LIST_SIZE; i++)
     {
@@ -25,21 +61,21 @@ int main()
             std::cout << "Enter a number: ";
             std::cin >> list[i];
         }
+        numElem++;
     }
-    for (int i = 0; i < LIST_SIZE; i++)
-    {
-        std::cout << list[i] << std::endl;
-    }
+    printList(list, numElem);
 
-    int largestPos = 0;
-    for (int i = 1; i < LIST_SIZE; i++)
-    {
-        if (list[largestPos] < list[i])
-        {
-            largestPos = i;
-        }
-    }
+    int largestPos;
+    findLargest(list, largestPos, numElem);
+
     std::cout << "The largest number is " << list[largestPos] << " at position " << largestPos << std::endl;
+    std::cout << list << std::endl;
+
+    sortList(list, numElem);
+    printList(list, numElem);
+
+    userTaskType userTask = ANDROID_STUDIO;
+    std::cout << userTask << " " << taskToStr[userTask] << std::endl;
 
     return 0;
 }
@@ -108,4 +144,36 @@ void binNumConvert()
         std::cout << list[i];
     }
     std::cout << " converted to decimal is " << convertedNum << std::endl;
+}
+
+void sortList(int list[], int listSize)
+{
+    int largestPos;
+    for (int unsorted = listSize - 1; unsorted > 0; unsorted--)
+    {
+        findLargest(list, largestPos, unsorted + 1);
+        int temp = list[unsorted];
+        list[unsorted] = list[largestPos];
+        list[largestPos] = temp;
+    }
+}
+
+void findLargest(int list[], int &largestPos, int listSize)
+{
+    largestPos = 0;
+    for (int i = 1; i < listSize; i++)
+    {
+        if (list[largestPos] < list[i])
+        {
+            largestPos = i;
+        }
+    }
+}
+
+void printList(int list[], int listSize)
+{
+    for (int i = 0; i < listSize; i++)
+    {
+        std::cout << list[i] << std::endl;
+    }
 }
