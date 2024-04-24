ddc6f48e7097a8c3a47782a80107ce9c612bdd70
Angela Venable <avenable11@ivytech.edu>
2024-04-24 14:41:47 -0400

    binary num version 3

--- 04_24_24/main.cpp
+++ 04_24_24/main.cpp
@@ -1,45 +1,13 @@
 #include <iostream>
 #include <limits>
 #include <cmath>
-#include <string>
 
 const int LIST_SIZE = 100;
 void resetStream();
+void binNumConvert();
 
 int main()
 {
-    int binaryDigit;
-    unsigned long convertedNum = 0;
-    int position = 0;
-    int list[LIST_SIZE];
-    std::string binNum;
-    std::cout << "Enter a binary number: ";
-    std::cin >> binNum;
-    std::cout << std::endl;
-    position = binNum.size() - 1;
-
-    for (int i = 0; i < binNum.size(); i++)
-    {
-        if (binNum[i] != '0' && binNum[i] != '1')
-        {
-            std::cout << "A binary digit can only be a 1 or 0." << std::endl;
-            break;
-        }
-        else
-        {
-            list[position] = binNum[i] - '0';
-            position--;
-        }
-    }
-    if (position == -1)
-    {
-        for (int i = 0; i < binNum.size(); i++)
-        {
-            convertedNum = convertedNum + list[i] * pow(2, i);
-        }
-    }
-
-    std::cout << binNum << " converted to decimal is " << convertedNum << std::endl;
 
     // standard array processing for loop
     /*  for (int i = 0; i < LIST_SIZE; i++)
@@ -54,4 +22,63 @@ void resetStream()
     std::cout << "You entered something that is not a number." << std::endl;
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+void binNumConvert()
+{
+    int binaryDigit;
+    unsigned long convertedNum = 0;
+    int position = 0;
+    int list[LIST_SIZE];
+    int exp = 0;
+
+    while (binaryDigit != -1)
+    {
+        std::cout << "Enter the " << position + 1;
+        if (position == 0)
+        {
+            std::cout << "st";
+        }
+        else if (position == 1)
+        {
+            std::cout << "nd";
+        }
+        else if (position == 2)
+        {
+            std::cout << "rd";
+        }
+        else
+        {
+            std::cout << "th";
+        }
+        std::cout << " binary digit (Enter -1 to complete the conversion): ";
+        std::cin >> binaryDigit;
+        if (binaryDigit == -1)
+        {
+            break;
+        }
+        if (!std::cin)
+        {
+            resetStream();
+            continue;
+        }
+        else if (binaryDigit != 0 && binaryDigit != 1)
+        {
+            std::cout << "A binary digit can only be a 1 or 0." << std::endl;
+            continue;
+        }
+
+        list[position] = binaryDigit;
+        position++;
+    }
+    for (int i = position - 1; i >= 0; i--)
+    {
+        convertedNum = convertedNum + list[i] * pow(2, exp);
+        exp++;
+    }
+    for (int i = 0; i < position; i++)
+    {
+        std::cout << list[i];
+    }
+    std::cout << " converted to decimal is " << convertedNum << std::endl;
 }
